5fbb5a69285800251806314305d7d4a9c4d1b323
austinvhuang <austinh@alum.mit.edu>
2020-08-16 22:17:04 -0400

    basic functionality working

--- src/Backend.hs
+++ src/Backend.hs
@@ -1,12 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-} {-# LANGUAGE RecordWildCards #-}
 
 module Backend where
+
+import GHC.Int (Int64)
+
+import System.Directory (copyFile)
 
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
 
 data Entry = Entry
-  { entryID :: Int,
+  { entryID :: Maybe Int, -- only needs a value when reading
     date :: String,
     time :: String,
     content :: String
@@ -17,25 +21,58 @@ instance FromRow Entry where
   fromRow = Entry <$> field <*> field <*> field <*> field
 
 data Tag = Tag
-  { foreignID :: Int,
+  { 
+    tagID :: Maybe Int, -- only needs a value when reading
+    foreignID :: Int,
     tag :: String
   }
   deriving (Show)
+
+instance FromRow Tag where
+  fromRow = Tag <$> field <*> field <*> field
 
 dbFile = "note2self.db"
 
 initDB :: IO ()
 initDB = do
+  copyFile dbFile (dbFile ++ ".backup")
   conn <- open dbFile
   execute_ conn "DROP TABLE IF EXISTS entries;"
   execute_ conn "DROP TABLE IF EXISTS tags;"
-  execute_ conn "CREATE TABLE entries (entryID INTEGER PRIMARY KEY, date text, time text, content text);"
-  execute_ conn "CREATE TABLE tags (entryID, tag text);"
+  execute_ conn "CREATE TABLE entries (entryID INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, time TEXT, content TEXT);"
+  execute_ conn "CREATE TABLE tags (tagID INTEGER PRIMARY KEY AUTOINCREMENT, entryID INTEGER, tag TEXT);"
   close conn
 
-dump = do
-  putStrLn "DB Contents"
+addEntry :: Entry -> IO Int64
+addEntry Entry{..} = do
+  conn <- open dbFile
+  executeNamed conn 
+    "INSERT INTO entries (date, time, content) VALUES (:date, :time, :content)" 
+    [":date" := date, ":time" := time, ":content" := content]
+  r <- lastInsertRowId conn
+  close conn
+  pure r
+
+addTag :: Int64 -> String -> IO Int64
+addTag entryID tag = do
+  conn <- open dbFile
+  executeNamed conn 
+    "INSERT INTO tags (entryID, tag) VALUES (:entryID, :tag)" 
+    [":entryID" := entryID, ":tag" := tag]
+  r <- lastInsertRowId conn
+  close conn
+  pure r
+
+dumpEntries = do
+  putStrLn "Entries ---"
   conn <- open dbFile
   r <- query_ conn "SELECT * from entries" :: IO [Entry]
+  mapM_ print r
+  close conn
+
+dumpTags = do
+  putStrLn "Tags ---"
+  conn <- open dbFile
+  r <- query_ conn "SELECT * from tags" :: IO [Tag]
   mapM_ print r
   close conn
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.Time
+
 import Options.Applicative
 
 import Backend
@@ -7,11 +9,17 @@ import Parser
 
 main :: IO ()
 main = do
-  initDB
   options <- execParser optionsParser
   print (options :: CommandLine)
   if (resetDB options)
-    then putStrLn "Resetting DB" >> initDB
+    then putStrLn "Resetting DB" >> initDB >> putStrLn "Previous DB saved as note2self.db.backup"
     else pure ()
-  dump
+
+  now <- getZonedTime
+  let dt = formatTime defaultTimeLocale "%Y-%m-%d" now
+  let tm = formatTime defaultTimeLocale "%H:%M:%S" now
+  r <- addEntry $ Entry Nothing dt tm (note options)
+  mapM (addTag r) (tags options)
+  dumpEntries
+  dumpTags
   pure ()
