727f3e9363730580bc55863b1f0c7f2adf67568e
austinvhuang <austinh@alum.mit.edu>
2021-01-23 15:28:19 -0500

    WIP requests from rust client

--- yew-test/src/lib.rs
+++ yew-test/src/lib.rs
@@ -1,30 +1,96 @@
+use anyhow::Error;
+use serde::Deserialize;
 use wasm_bindgen::prelude::*;
-use yew::prelude::*;
+use yew::format::{Json, Nothing};
+use yew::services::fetch::{FetchService, FetchTask, Request, Response};
+use yew::*;
 
-struct Model {
+pub struct App {
+    fetch_task: Option<FetchTask>,
+    entries: Option<Entries>,
     link: ComponentLink<Self>,
-    value: i64,
+    error: Option<String>,
 }
 
-enum Msg {
-    AddOne,
+#[derive(Debug)]
+pub enum Msg {
+    GetEntries,
+    ReceiveEntries(Result<Entries, anyhow::Error>),
 }
 
-impl Component for Model {
+#[derive(Deserialize, Debug, Clone)]
+pub struct Entries {
+    entries: Vec<Entry>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Entry {
+    time: String,
+    entry_id: i32,
+    content: String,
+    date: String,
+}
+
+impl App {
+    fn view_entries(&self) -> Html {
+        html! { <div> {"Fetched"} </div> }
+    }
+    fn view_fetching(&self) -> Html {
+        html! { <div> {"Fetching..."} </div> }
+    }
+    fn view_error(&self) -> Html {
+        html! { <div> {"Error"} </div> }
+    }
+}
+
+impl Component for App {
     type Message = Msg;
     type Properties = ();
+
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
+            fetch_task: None,
+            entries: None,
             link,
-            value: 0,
+            error: None,
         }
     }
 
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddOne => self.value +=1
+            GetEntries => {
+                // define request
+                let request = Request::get("http://localhost:3000/all/entires")
+                    .body(Nothing)
+                    .expect("Could not build request.");
+                // define callback
+                // TODO fix From trait error
+                /*
+                let callback =
+                    self.link
+                        .callback(|response: Response<Json<Result<Entries, anyhow::Error>>>| {
+                            let Json(data) = response.into_body();
+                            Msg::ReceiveEntries(data);
+                        });
+                // pass request+callback to fetch service
+                let task = FetchService::fetch(request, callback).expect("failed to start request");
+                self.fetch_task = Some(task);
+                */
+                true // redraw page
+            }
+            Msg::ReceiveEntries(response) => {
+                match response {
+                    Ok(result) => {
+                        self.entries = Some(result);
+                    }
+                    Err(error) => {
+                        self.error = Some(error.to_string());
+                    }
+                }
+                self.fetch_task = None;
+                true // redraw page
+            }
         }
-        true
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -33,9 +99,25 @@ impl Component for Model {
 
     fn view(&self) -> Html {
         html! {
-            <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
+            <div class="main">
+                <h1>
+                    { "Note2Self" }
+                </h1>
+                <hr/>
+                <p/>
+                <input type="text" class= "search-input"/>
+                <p/>
+                <div class="card">
+                    { "Hey" }
+                </div>
+                <div class="card">
+                    { "There" }
+                </div>
+                    <>
+                        { self.view_fetching() }
+                        { self.view_entries() }
+                        { self.view_error() }
+                    </>
             </div>
         }
     }
@@ -43,6 +125,5 @@ impl Component for Model {
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
-    App::<Model>::new().mount_to_body();
+    yew::start_app::<App>();
 }
-
