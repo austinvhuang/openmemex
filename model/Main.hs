cbc07d1f765218738f91727036d718f74c87a0de
austinvhuang <austinh@alum.mit.edu>
2021-04-05 22:57:22 -0400

    roberta tracing export (exports a file but results arent consistent with original model), and haskell torchscript loading test (still need to figure out how to unpack a tuple

--- model/Main.hs
+++ model/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import Torch
 import Torch.Script 
 
-main = do
+mlpTest = do
   model <- load WithoutRequiredGrad "traced.zip"
   let x               = asTensor ([[1.0, 2.0]] :: [[Float]])
       IVTensor result = Torch.Script.forward model [IVTensor x]
@@ -11,3 +11,16 @@ main = do
   putStrLn $ "\nInput\n" ++ show x
   putStrLn $ "\nTorscript Inference\n" ++ show result
   putStrLn $ "\nExpected\n" ++ show expected
+
+robertaTest :: IO ()
+robertaTest = do
+  model <- load WithoutRequiredGrad "roberta_traced.zip"
+  let ids = asTensor([[ 0, 3592, 141, 32, 47, 608, 4, 42, 16, 10, 1296, 4, 2]] :: [[Int]])
+  let IVTensor result = Torch.Script.forward model [IVTensor ids] -- error this should be a tuple
+  -- let IVTuple (IVTensor lastHiddenState) (IVTensor poolerOutput) = Torch.Script.forward model [IVTensor ids]
+  putStrLn $ "\nInput\n" ++ show ids
+  putStrLn $ "\nTorscript Inference\n" ++ show result
+            
+  pure ()
+
+main = robertaTest
--- model/test_export.py
+++ model/test_export.py
@@ -1,5 +1,7 @@
 import torch
-from transformers import RobertaTokenizer, RobertaConfig, RobertaModel
+from transformers import RobertaTokenizerFast, RobertaConfig, RobertaModel
+import umap
+import altair
 
 class Model(torch.nn.Module):
     def __init__(self):
@@ -11,19 +13,41 @@ class Model(torch.nn.Module):
     def forward(self, x):
         return self.fc1(x)
 
-if __name__ == "__main__":
-    # test = Model()
-    # traced = torch.jit.trace(test, torch.ones([1, 2]))
-    # traced.save('traced.zip')
+def jit_test():
+    test = Model()
+    traced = torch.jit.trace(test, torch.ones([1, 2]))
+    traced.save('traced.zip')
 
-    tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
+def jit_roberta():
+    # tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
+    tokenizer = RobertaTokenizerFast.from_pretrained("roberta-base")
     configuration = RobertaConfig()
     model = RobertaModel(configuration)
-    tokenizer("Hello world")['input_ids']
-    tokens = tokenizer("Hello world. This is a test sentence")
-    tokens = tokenizer(["Hello world. This is a test sentence", "and this is another sentence", "hi i am here"])
-    ids = torch.tensor(tokens['input_ids'][0]).reshape(1, 10)
+    tokens = tokenizer("hi how are you doing. this is a test.")
+    ids = torch.tensor(tokens['input_ids']).reshape(1, len(tokens['input_ids']))
+    (last_hidden_state, pooler_output) = model(ids)
+    traced = torch.jit.trace(model, ids)
+    (last_hidden_state_t, pooler_output_t) = traced(ids)
+    diffs_lhs, diffs_pool = (last_hidden_state_t - last_hidden_state, pooler_output_t - pooler_output)
+    # these diffs don't look good.
+    traced.save('roberta_traced.zip')
 
-    None
+
+if __name__ == "__main__":
+    # jit_test()
+    # jit_roberta()
+
+    tokenizer = RobertaTokenizerFast.from_pretrained("roberta-base")
+    configuration = RobertaConfig()
+    model = RobertaModel(configuration)
+    tokens = tokenizer("hi how are you doing. this is a test.")
+    ids = torch.tensor(tokens['input_ids']).reshape(1, len(tokens['input_ids']))
+    (last_hidden_state, pooler_output) = model(ids)
+    traced = torch.jit.trace(model, ids)
+    (last_hidden_state_t, pooler_output_t) = traced(ids)
+    diffs_lhs, diffs_pool = (last_hidden_state_t - last_hidden_state, pooler_output_t - pooler_output)
+
+    print(ids)
+    print(traced(ids))
 
 
