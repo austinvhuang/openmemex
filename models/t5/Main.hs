f0d31ec3e5e22d0c2571fe0f2034caf8b892c934
austinvhuang <austinh@alum.mit.edu>
2021-06-06 21:43:58 -0400

    haskell t5 cleanup

--- models/t5/Main.hs
+++ models/t5/Main.hs
@@ -7,21 +7,16 @@ main = do
 
   -- load example data
   S.IVGenericDict example <- pickleLoad "traced_t5-small.example.pt"
-  let inputIDs = snd $ example !! 0
-  let inputsAttMask = snd $ example !! 1
-  let decoderInputs = snd $ example !! 2
-  let decoderAttMask = snd $ example !! 3
+  let [inputIDs , inputsMask, decoderIDs, decoderMask] = snd <$> example
 
   -- load torchscript module
   tsModule <- S.loadScript WithoutRequiredGrad "traced_t5-small.pt"
   
   -- perform inference computation
-  let IVTuple result = Torch.forward tsModule [inputIDs, inputsAttMask, decoderInputs, decoderAttMask] 
-  let IVTensor res0 = result !! 0 
-  let IVTuple res1 = result !! 1
-  let res1' = (\(IVTuple x) -> x) <$> res1
-  let IVTensor res2 = result !! 2
-  print $ res0 ! 0 ! 0 ! 0
-  -- print result
+  let IVTuple [IVTensor r0, IVTuple r1, IVTensor r2] =
+        Torch.forward tsModule [inputIDs, inputsMask, decoderIDs, decoderMask] 
+    
+  print $ r0
 
   putStrLn "Done"
+
